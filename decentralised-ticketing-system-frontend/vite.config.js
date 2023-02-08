8808249fccb1e3084280aefbc57eb65417266018
kotka <monibgr@gmail.com>
2023-02-08 20:22:17 +0200

    Package.json fix and build chunk size fix

--- decentralised-ticketing-system-frontend/vite.config.js
+++ decentralised-ticketing-system-frontend/vite.config.js
@@ -7,7 +7,16 @@ export default defineConfig({
     port: 3000,
   },
   build: {
-      outDir: 'build',
+    outDir: 'build',
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
   },
   plugins: [react({ plugins: [["@swc/plugin-styled-components", {}]] })],
 })

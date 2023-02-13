26fc456ec71d9ad4de2156ae5585e114fd23a096
kotka <monibgr@gmail.com>
2023-02-13 16:12:38 +0200

    More attempts to fix walletconnect

--- decentralised-ticketing-system-frontend/vite.config.js
+++ decentralised-ticketing-system-frontend/vite.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 export default defineConfig({
   optimizeDeps: {
       esbuildOptions: {
           define: {
-              global: 'globalThis'
-          },
-          plugins: [
-              NodeGlobalsPolyfillPlugin({
-                  buffer: true
-              })
-          ]
-      }
+              global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }), 
+        NodeModulesPolyfillPlugin() 
+      ]
+    },
   },
   server: {
     port: 3000,
@@ -20,14 +23,19 @@ export default defineConfig({
   build: {
     outDir: 'build',
     rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
         }
-    }
+      },
+      plugins: [
+        rollupNodePolyFill()
+      ]
+    },
   },
-  plugins: [react({ plugins: [["@swc/plugin-styled-components", {}]] })],
+  plugins: [
+    react({ plugins: [["@swc/plugin-styled-components", {}]] }),
+  ],
 })

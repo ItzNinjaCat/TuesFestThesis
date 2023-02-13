0d73d5ecb5f920f15cf2324d07dfeae6f69973a7
kotka <monibgr@gmail.com>
2023-02-13 13:00:40 +0200

    WalletConnect polyfill fix

--- decentralised-ticketing-system-frontend/src/utils/chains.js
+++ decentralised-ticketing-system-frontend/src/utils/chains.js
@@ -18,129 +18,18 @@ const CELO = {
 
 export const CHAINS = {
     1: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://mainnet.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-            import.meta.env.VITE_ALCHEMY_KEY
-                ? `https://eth-mainnet.alchemyapi.io/v2/${import.meta.env.VITE_ALCHEMY_KEY}`
-                : '',
-            'https://cloudflare-eth.com',
-        ].filter(url => url !== ''),
-        name: 'Mainnet',
-    },
-    3: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://ropsten.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-        ].filter(url => url !== ''),
-        name: 'Ropsten',
-    },
-    4: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://rinkeby.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-        ].filter(url => url !== ''),
-        name: 'Rinkeby',
+    urls: [
+      import.meta.env.infuraKey ? `https://mainnet.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}` : '',
+      import.meta.env.alchemyKey ? `https://eth-mainnet.alchemyapi.io/v2/${import.meta.env.VITE_ALCHEMY_KEY}` : '',
+      'https://cloudflare-eth.com',
+    ].filter((url) => url !== ''),
+    name: 'Mainnet',
     },
     5: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://goerli.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-        ].filter(url => url !== ''),
-        name: 'Görli',
-    },
-    42: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://kovan.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-        ].filter(url => url !== ''),
-        name: 'Kovan',
-    },
-    // Optimism
-    10: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://optimism-mainnet.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-            'https://mainnet.optimism.io',
-        ].filter(url => url !== ''),
-        name: 'Optimism',
-        nativeCurrency: ETH,
-        blockExplorerUrls: ['https://optimistic.etherscan.io'],
-    },
-    69: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://optimism-kovan.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-            'https://kovan.optimism.io',
-        ].filter(url => url !== ''),
-        name: 'Optimism Kovan',
-        nativeCurrency: ETH,
-        blockExplorerUrls: ['https://kovan-optimistic.etherscan.io'],
-    },
-    // Arbitrum
-    42161: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://arbitrum-mainnet.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-            'https://arb1.arbitrum.io/rpc',
-        ].filter(url => url !== ''),
-        name: 'Arbitrum One',
-        nativeCurrency: ETH,
-        blockExplorerUrls: ['https://arbiscan.io'],
-    },
-    421611: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://arbitrum-rinkeby.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-            'https://rinkeby.arbitrum.io/rpc',
-        ].filter(url => url !== ''),
-        name: 'Arbitrum Testnet',
-        nativeCurrency: ETH,
-        blockExplorerUrls: ['https://testnet.arbiscan.io'],
-    },
-    // Polygon
-    137: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://polygon-mainnet.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-            'https://polygon-rpc.com',
-        ].filter(url => url !== ''),
-        name: 'Polygon Mainnet',
-        nativeCurrency: MATIC,
-        blockExplorerUrls: ['https://polygonscan.com'],
-    },
-    80001: {
-        urls: [
-            import.meta.env.VITE_INFURA_KEY
-                ? `https://polygon-mumbai.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}`
-                : '',
-        ].filter(url => url !== ''),
-        name: 'Polygon Mumbai',
-        nativeCurrency: MATIC,
-        blockExplorerUrls: ['https://mumbai.polygonscan.com'],
-    },
-    // Celo
-    42220: {
-        urls: ['https://forno.celo.org'],
-        name: 'Celo',
-        nativeCurrency: CELO,
-        blockExplorerUrls: ['https://explorer.celo.org'],
-    },
-    44787: {
-        urls: ['https://alfajores-forno.celo-testnet.org'],
-        name: 'Celo Alfajores',
-        nativeCurrency: CELO,
-        blockExplorerUrls: ['https://alfajores-blockscout.celo-testnet.org'],
+    urls: [import.meta.env.infuraKey ? `https://goerli.infura.io/v3/${import.meta.env.VITE_INFURA_KEY}` : ''].filter(
+      (url) => url !== ''
+    ),
+    name: 'Görli',
     },
 };
 
--- decentralised-ticketing-system-frontend/vite.config.js
+++ decentralised-ticketing-system-frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 export default defineConfig({
+  optimizeDeps: {
+      esbuildOptions: {
+          define: {
+              global: 'globalThis'
+          },
+          plugins: [
+              NodeGlobalsPolyfillPlugin({
+                  buffer: true
+              })
+          ]
+      }
+  },
   server: {
     port: 3000,
   },
